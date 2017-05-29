d37648b1a38108f34ddbf7fb0171c1d0950edf1b
indsolsg1 <indsolsg1@VM001.wkgfjte3xeze5nscffixjlegwe.ix.internal.cloudapp.net>
2017-05-29 14:20:30 +0000

    Adding the rest

---
+++ src/selenium/WebDriverFactory.java
@@ -0,0 +1,26 @@
+package selenium;
+
+import config.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.ie.*;
+
+public class WebDriverFactory {
+	WebDriver WD;
+	public WebDriverFactory(String choice){
+		switch (choice.toLowerCase()){
+			case "chrome":
+				System.setProperty("webdriver.chrome.driver",Props.getProperty("CHROME_DRIVER_SERVER_PATH"));
+				WD = new ChromeDriver();
+				break;
+			case "ie":
+				System.setProperty("webdriver.ie.driver",Props.getProperty("IE_DRIVER_SERVER_PATH"));
+				WD = new InternetExplorerDriver();
+				break;
+		}
+	}
+	
+	public WebDriver getWebDriver(){
+		return WD;
+	}
+}
