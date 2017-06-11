efbb660236abe9350b4de5304938682f803c2cea
indsolsg1 <indsolsg1@VM001.wkgfjte3xeze5nscffixjlegwe.ix.internal.cloudapp.net>
2017-06-11 12:15:50 +0000

    Updated Hooks and Step Def files for DI

--- src/test/java/cucumbertestcases/steps/Hooks.java
+++ src/test/java/cucumbertestcases/steps/Hooks.java
@@ -5,25 +5,31 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import test.java.automationframework.WebDriverFactory;
 
-public class Hooks{
-	public static WebDriver WD;
+public class Hooks {
+	//public static WebDriver WD;
+	BaseUtil base;
+	
+	public Hooks(BaseUtil base){
+		this.base = base;
+	}
+	
 	
 	@Before("@seleniumchrome")
 	public void seleniumchrome(){
-		WD = new WebDriverFactory("chrome").getWebDriver();
-		WD.manage().window().maximize();
-		WD.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+		base.WD = new WebDriverFactory("chrome").getWebDriver();
+		base.WD.manage().window().maximize();
+		base.WD.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
 	}
 	
 	@Before("@seleniumie")
 	public void seleniumie(){
-		WD = new WebDriverFactory("ie").getWebDriver();
-		WD.manage().window().maximize();
-		WD.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+		base.WD = new WebDriverFactory("ie").getWebDriver();
+		base.WD.manage().window().maximize();
+		base.WD.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
 	}
 	
 	@After("@seleniumchrome,@seleniumie")
 	public void seleniumteardown(){
-		WD.quit();
+		base.WD.quit();
 	}
 }
--- src/test/java/cucumbertestcases/steps/StepDefinitionGoogleNavigation.java
+++ src/test/java/cucumbertestcases/steps/StepDefinitionGoogleNavigation.java
@@ -2,22 +2,29 @@ package test.java.cucumbertestcases.steps;
 import cucumber.api.java.en.*;
 import test.java.pageobjects.SearchForm;
 import test.java.pageobjects.TopNavigation;
-public class StepDefinitionGoogleNavigation {
+public class StepDefinitionGoogleNavigation{
+	BaseUtil base;
+	
+	public StepDefinitionGoogleNavigation(BaseUtil base){
+		this.base = base;
+	}
+	
 	@Given("^User is at Page \"([^\"]*)\"$")
 	public void user_is_at_Page(String arg1) throws Throwable {
-		Hooks.WD.get(arg1);
+		System.out.println("Zhongren Ma go eat shit!");
+		base.WD.get(arg1);
 	}
 	@When("^User enters \"([^\"]*)\" in search textfield$")
 	public void user_enters_in_search_textfield(String arg1) throws Throwable {
-		SearchForm.inputQ(Hooks.WD).sendKeys(arg1);
+		SearchForm.inputQ(base.WD).sendKeys(arg1);
 	}
 	@And("^User clicks on search button$")
 	public void user_clicks_on_search_button() throws Throwable {
-		SearchForm.buttonSearch(Hooks.WD).click();
+		SearchForm.buttonSearch(base.WD).click();
 	}
 	@And("^User clicks on images hyperlink$")
 	public void userClicksOnImagesHyperlink() throws Throwable {
-		TopNavigation.aImages(Hooks.WD).click();
+		TopNavigation.aImages(base.WD).click();
 	}
 	@Then("^images will be shown in grid format$")
 	public void imagesWillBeShownInGridFormat() throws Throwable {
